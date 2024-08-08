81b7aaf286cf669e0d0110d719897d55fc88c6ed
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-08 15:18:42 +0300

    Add project files.

---
+++ InheritanceExample/BaseKisi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    public abstract class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void DisplayInfo()
+
+        {
+            Console.WriteLine($"Ad: {Ad}\nSoyad:{Soyad}");
+
+        }
+
+
+    }
+
+    public class Ogrenci : BaseKisi
+
+    {
+
+        public string OgrenciNo { get; set; }
+
+
+        public Ogrenci(string ad, string soyad)
+
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+
+        public void DisplayNumber(string ogrenciNo)
+        {
+            OgrenciNo = ogrenciNo;
+            DisplayInfo();
+            Console.WriteLine(ogrenciNo);
+
+        }
+    }
+
+    public class Ogretmen : BaseKisi
+    {
+
+
+        public Ogretmen(string ad, string soyad)
+
+        {
+            Ad = ad;
+            Soyad = soyad;
+
+        }
+        private int Salary { get; set; }
+        public void DisplaySalary(int salary)
+        {
+            Salary = salary;
+            DisplayInfo();
+            Console.WriteLine($"Maas:{salary}");
+
+        }
+    }
+}
---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,7 @@
+﻿using InheritanceExample;
+
+Ogrenci ogrenci = new Ogrenci("Ali","Veli");
+ogrenci.DisplayNumber("123456987");
+
+Ogretmen ogretmen = new Ogretmen("Cemil", "Suat");
+ogretmen.DisplaySalary(5000);
